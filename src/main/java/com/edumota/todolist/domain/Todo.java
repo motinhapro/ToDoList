9b41c4d7b3595bb23b40be9e140e9f0c3ffc7546
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-07-16 17:11:35 -0300

    feat: add Todo entity

---
+++ src/main/java/com/edumota/todolist/domain/Todo.java
@@ -0,0 +1,36 @@
+package com.edumota.todolist.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime dueDate;
+
+    public Todo() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Todo(Long id, String title, String description, LocalDateTime dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+        this.dueDate = dueDate;
+    }
+}
